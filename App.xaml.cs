9829e4711beba109a30faa294f0cd174a8ac2583
zzz <gjxyr@outlook.com>
2024-03-12 17:44:39 +0800

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+using System.Windows.Threading;
+using MaterialDemo.Services;
+
+
+namespace MaterialDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private static readonly IHost _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<ApplicationHostService>();
+
+                // Main window with navigation
+                services.AddSingleton<MainWindow>();
+
+            }).Build();
+
+
+        public static T GetService<T>()
+         where T : class
+        {
+            return _host.Services.GetService(typeof(T)) as T;
+        }
+
+        /// <summary>
+        /// Occurs when the application is loading.
+        /// </summary>
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            _host.Start();
+        }
+
+        /// <summary>
+        /// Occurs when the application is closing.
+        /// </summary>
+        private async void OnExit(object sender, ExitEventArgs e)
+        {
+            await _host.StopAsync();
+
+            _host.Dispose();
+        }
+
+        /// <summary>
+        /// Occurs when an exception is thrown by an application but not handled.
+        /// </summary>
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
+        }
+    }
+
+}
---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace MaterialDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Window methods
+
+        //public INavigationView GetNavigation() => RootNavigation;
+
+        //public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
+
+        //public void SetPageService(IPageService pageService) => RootNavigation.SetPageService(pageService);
+
+        public void ShowWindow() => base.Show();
+
+        public void CloseWindow() => base.Close();
+
+        #endregion Window methods
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Make sure that closing this window will begin the process of closing the application.
+            Application.Current.Shutdown();
+        }
+
+        public void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
