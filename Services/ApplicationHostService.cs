95ddc46434c57d990180ad54941dea2291bf0584
zzz <gjxyr@outlook.com>
2024-03-12 17:44:39 +0800

    3/14

---
+++ Services/ApplicationHostService.cs
@@ -0,0 +1,56 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Microsoft.Extensions.Hosting;
+
+
+namespace MaterialDemo.Services
+{
+    /// <summary>
+    /// Managed host of the application.
+    /// </summary>
+    public class ApplicationHostService(IServiceProvider serviceProvider) : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+        private MainWindow _mainWindow;
+
+        /// <summary>
+        /// Triggered when the application host is ready to start the service.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the start process has been aborted.</param>
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await HandleActivationAsync();
+        }
+
+        /// <summary>
+        /// Triggered when the application host is performing a graceful shutdown.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Creates main window during activation.
+        /// </summary>
+        private async Task HandleActivationAsync()
+        {
+            if (!Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                _mainWindow = (
+                    _serviceProvider.GetService(typeof(MainWindow)) as MainWindow
+                )!;
+                _mainWindow!.ShowWindow();
+
+                //_mainwindow.navigate(typeof(views.pages.dashboardpage));
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿namespace MaterialDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Window methods
+
+        //public INavigationView GetNavigation() => RootNavigation;
+
+        //public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
+
+        //public void SetPageService(IPageService pageService) => RootNavigation.SetPageService(pageService);
+
+        public void ShowWindow() => base.Show();
+
+        public void CloseWindow() => base.Close();
+
+        #endregion Window methods
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Make sure that closing this window will begin the process of closing the application.
+            Application.Current.Shutdown();
+        }
+
+        public void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void username_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            usernameBox.Focus();
+        }
+
+        private void usernameBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(usernameBox.Text))
+            {
+                username.Visibility = Visibility.Collapsed;
+            } else {
+                username.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void password_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            passwordBox.Focus();
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(passwordBox.Password))
+            {
+                password.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                password.Visibility = Visibility.Visible;
+            }
+        }
+    }
+
+}
