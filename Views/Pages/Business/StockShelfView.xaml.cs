db02fb2ff33788cffe93942b5866d6b0375842d2
zheng.zz <gjxyr@outlook.com>
2024-05-24 23:18:35 +0800

    524 1119

---
+++ Views/Pages/Business/StockShelfView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MaterialDemo.ViewModels.Pages.Business;
+using Wpf.Ui.Controls;
+
+namespace MaterialDemo.Views.Pages.Business
+{
+    public partial class StockShelfView : INavigableView<StockShelfViewModel>
+    {
+        public StockShelfViewModel ViewModel { get; }
+
+        public StockShelfView(StockShelfViewModel viewModel)
+        {
+            this.ViewModel = viewModel;
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
