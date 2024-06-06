9e3a97366dfe859847f94be881cb81a061988196
zheng.zz <gjxyr@outlook.com>
2024-06-07 00:05:05 +0800

    6071205

---
+++ ViewModels/Pages/Business/VObject/StockIndexItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialDemo.ViewModels.Pages.Business.VObject
+{
+    public class StockIndexItem
+    {
+        private readonly string Id;
+
+        private readonly string Name;
+
+        private readonly string Code;
+
+        private readonly string Model;
+
+        private volatile int quantity;
+
+
+
+    }
+}
